c37c724f5af4aecf3e3611f4836f38b4271765b8
Subhash <59171525+Subbu636@users.noreply.github.com>
2021-04-22 22:12:05 +0530

    Setup and Structure

---
+++ util.h
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Point{
+    public:
+    double x, y;
+    double distance(Point p);
+};
+
+void read_file(char *filename, vector <Point> &points);
+
