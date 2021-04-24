de861739b927d1447fff57c72c45c8cd1f935f8a
Mooizz <albusdumbledore8918@gmail.com>
2021-04-24 19:03:14 +0530

    Added distance update

--- KMeans.cu
+++ KMeans.cu
@@ -6,9 +6,19 @@ vector <Point> kmeans_cpu(vector <Point> points, int iter){
     return means;
 } 
 
-
-vector <Point> kmeans_gpu(vector <Point> points, int iter){
-    vector <Point> means;
-    // write kmeans gpu 
-    return means;
+__global__ void distance_update(double* dist, Points* points, Points* mean)
+{
+    int id = (blockIdx.x*blockDim.x) + threadIdx.x;
+    int i = blockIdx.x;
+    int j = threadIdx.x;
+    Point a = points[i];
+    Point b = mean[i];
+    double xdist = (a.x - b.x)*(a.x - b.x);
+    double ydist = (a.y - b.y)*(a.y - b.y);
+    dist[id] = xdist + ydist;
+}
+void kmeans_gpu(Points* points, Points* means,int* labels,double* dist, int iter, int n, int k){
+    // distances size n*k 
+    // write kmeans gpu  
+    distance_update<<<n,k>>>(dist,points,mean);
 } 
