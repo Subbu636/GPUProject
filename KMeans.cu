addf7468cdd46c974917b42f04adf34b53ef3c29
Mooizz <albusdumbledore8918@gmail.com>
2021-04-25 12:04:35 +0530

    Added CPU Code

--- KMeans.cu
+++ KMeans.cu
@@ -1,18 +1,69 @@
 #include "util.h"
 
-vector <Point> kmeans_cpu(vector <Point> points, int iter){
-    vector <Point> means;
+void dist_update(double* dist, Point* points, Point* means, int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<k;j++)
+        {
+            Point a = points[i];
+            Point b = means[j];
+            double xdist = (a.x - b.x)*(a.x - b.x);
+            double ydist = (a.y - b.y)*(a.y - b.y);
+            dist[i*n + j] = xdist + ydist;
+        }
+    }
+}
+void label_update_cpu(int* labels, double* dist, int k,int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        int l = 0;
+        double mindist = dist[i*n]; 
+        for(int j=1;j<k;j++)
+        {
+            if(mindist > dist[i*n + j]){
+                l = j;
+                mindist = dist[i*n + j];
+            }
+        }
+        labels[i] = l;
+    }
+}
+void centers_update_cpu(Point* means,Point* points,int* labels,int n,int k)
+{
+    for(int j=0;j<k;j++)
+    {
+        means[j].x = 0;
+        means[j].y = 0;
+        double num_points = 0;
+        for(int i=0;i<n;i++){
+            if(labels[i]==j)
+            {
+                num_points+=1;
+                means[j].x += points[i].x;
+                means[j].y += points[i].y;
+            }
+        }
+        means[j].x /= num_points;
+        means[j].y /= num_points;
+    }
+}
+void kmeans_cpu(Point* points, Point* means,int* labels,double* dist, int iter, int n, int k){
+    
     // write kmeans cpu 
-    return means;
+    dist_update(dist,points,means,n);
+    label_update_cpu(labels,dist,k,n);
+    centers_update_cpu(means,points,labels,n,k);
 } 
 
-__global__ void distance_update(double* dist, Points* points, Points* mean)
+__global__ void distance_update(double* dist, Point* points, Point* means)
 {
     int id = (blockIdx.x*blockDim.x) + threadIdx.x;
     int i = blockIdx.x;
     int j = threadIdx.x;
     Point a = points[i];
-    Point b = mean[i];
+    Point b = means[j];
     double xdist = (a.x - b.x)*(a.x - b.x);
     double ydist = (a.y - b.y)*(a.y - b.y);
     dist[id] = xdist + ydist;
@@ -33,7 +84,7 @@ __global__ void label_update(int* labels,double* dist,int k,int n)
         labels[id] = l;
     }
 }
-__global__ void centers_update(Points* means,Points* points,int* labels,int n,int k)
+__global__ void centers_update(Point* means,Point* points,int* labels,int n,int k)
 {
     int id = threadIdx.x;
     if(id<k){
@@ -52,7 +103,7 @@ __global__ void centers_update(Points* means,Points* points,int* labels,int n,in
         means[id].y /= num_points;
     }
 }
-void kmeans_gpu(Points* points, Points* means,int* labels,double* dist, int iter, int n, int k){
+void kmeans_gpu(Point* points, Point* means,int* labels,double* dist, int iter, int n, int k){
     // distances size n*k 
     // write kmeans gpu  
     distance_update<<<n,k>>>(dist,points,mean);
