5121bebe25ab09d87818aeab52217e3b40afd6c9
Subhash <59171525+Subbu636@users.noreply.github.com>
2021-05-03 17:47:48 +0530

    GMix Complete

--- util.h
+++ util.h
@@ -3,11 +3,12 @@
 #include <bits/stdc++.h>
 #include <cuda_runtime.h>
 #include <sys/time.h> 
+#include "cublas_v2.h"
 using namespace std;
 
-typedef struct Point{
+struct Point{
     double x, y;
-}Point;
+};
 
 void read_file(char *filename, vector <Point> &points);
 
@@ -15,7 +16,15 @@ vector <Point> kmeans_cpu(vector <Point> points, int iter);
 
 vector <Point> kmeans_gpu(vector <Point> points, int iter);
 
-vector <vector <double>> gmix_gpu(vector <Point> points, int iter);
 
-vector <vector <double>> gmix_cpu(vector <Point> points, int iter);
+// Gmix Functions
+double *read_file_array(char *fname, int d,int *l);
+__host__ __device__ double *create(int m, int n);
+double *init_prob(int k, int l);
+void gmix_cpu(double *p,double *r, int k, int iter, int l, int d, double *ctime);
+void gmix_gpu(double *p,double *r, int k, int iter, int l, int d, double *gtime);
+void fprint_mat(char *fname, double *prob, int l, int k);
+
+
+
 
